86ac42aca4a6a6907ea27a4a3c27417f384b83ce
Ahmad Mujtaba <cadeta6@gmail.com>
2022-11-13 02:34:32 +0500

    build{WelcomeScreen.dart}: converted to statefulwidget

--- lib/view/WelcomeScreen.dart
+++ lib/view/WelcomeScreen.dart
@@ -4,7 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/view/quizz_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_application_1/Component/navbarBottom.dart';
-class WelcomeScreen extends StatelessWidget{
+class WelcomeScreen extends StatefulWidget{
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
   return Scaffold(
@@ -16,35 +21,43 @@ class WelcomeScreen extends StatelessWidget{
         SafeArea(
           child:Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              Spacer(flex:1),
-              Text("Welcome to Quizz App",
-              style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.black, fontWeight: FontWeight.bold),
-              ),
-              
-              Text("Enter your name", style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.black, fontWeight: FontWeight.bold)),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Full Name"
-              ),),
-              ElevatedButton(onPressed: ()
-              {Navigator.push(context, MaterialPageRoute(
-                builder: ((context)=>MaterialApp(
-                  home: HomeBar(),
-                ))
+          child: Form(
+            child: Column(
+              children: [
+                Spacer(flex:1),
+                SizedBox(height: 100,),
+                Text("Welcome to Quiz App",
+                style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.black87, fontWeight: FontWeight.bold),
                 ),
-                );
-              },
-              child: Text("Let's Start Quiz"),
-              style: ElevatedButton.styleFrom(primary: Colors.black),),
-              Spacer(flex: 1),
-            ],)
+                SizedBox(height: 20,),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Username"
+                ),),
+                SizedBox(height: 20,),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Password"
+                ),
+                ),
+                SizedBox(height: 20,),
+                ElevatedButton(onPressed: ()
+                {Navigator.push(context, MaterialPageRoute(
+                  builder: ((context)=>MaterialApp(
+                    home: HomeBar(),
+                  ))
+                  ),
+                  );
+                },
+                child: Text("Let's Start Quiz"),
+                style: ElevatedButton.styleFrom(primary: Colors.black87),),
+                Spacer(flex: 1),
+              ],),
+          )
             )
             )
       ],
     )
   );
   }
-
 }
