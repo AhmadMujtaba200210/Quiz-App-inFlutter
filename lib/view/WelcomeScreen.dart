1104bd8da4a6e2722416c69be2c5fac28f5b90c7
Ahmad Mujtaba <cadeta6@gmail.com>
2022-11-15 22:21:20 +0500

    feat{WelcomeScreen.dart}: form validation added in login page

--- lib/view/WelcomeScreen.dart
+++ lib/view/WelcomeScreen.dart
@@ -12,6 +12,7 @@ class WelcomeScreen extends StatefulWidget{
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   return Scaffold(
     body: Stack(
       children: [
@@ -21,28 +22,46 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         SafeArea(
           child:Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Form(
+            child: Form(
+              key: formKey,
             child: Column(
               children: [
                 Spacer(flex:1),
                 SizedBox(height: 100,),
-                Text("Welcome to Quiz App",
+                Text("Want to Invest?",
+                style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.black87, fontWeight: FontWeight.bold),
+                ),
+                Text("Welcomed !",
                 style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.black87, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 20,),
-                TextField(
+                TextFormField(
                   decoration: InputDecoration(
                     hintText: "Username"
-                ),),
+                ),
+                  validator: (value) {
+                    if(value==null||value.isEmpty){
+                      return 'Please enter username';
+                    }
+                  },
+                ),
                 SizedBox(height: 20,),
-                TextField(
+                TextFormField(
                   decoration: InputDecoration(
                     hintText: "Password"
                 ),
+                validator: (value) {
+                    if (value == null || value.isEmpty) {
+                        return 'Please enter Password';
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(height: 20,),
                 ElevatedButton(onPressed: ()
-                {Navigator.push(context, MaterialPageRoute(
+                {
+                  if(formKey.currentState!.validate())
+                  Navigator.push(context, MaterialPageRoute(
                   builder: ((context)=>MaterialApp(
                     home: HomeBar(),
                   ))
